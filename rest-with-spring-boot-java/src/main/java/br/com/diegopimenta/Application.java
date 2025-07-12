eded82e07a584e57c129da4c714d150a897457f1
Diego Santos <diegopimenta.ds@hotmail.com>
2025-07-11 21:26:51 -0300

    start with configs

---
+++ rest-with-spring-boot-java/src/main/java/br/com/diegopimenta/Application.java
@@ -0,0 +1,13 @@
+package br.com.diegopimenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
